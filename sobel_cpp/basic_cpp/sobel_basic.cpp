00e8a01a5b3337dc6cff60efba96bf51bc3cbb2d
Joseph Loy <thejosephloy@gmail.com>
2023-05-19 09:50:04 -0500

    basics cpp adds

--- sobel_cpp/basic_cpp/sobel_basic.cpp
+++ sobel_cpp/basic_cpp/sobel_basic.cpp
@@ -1,1 +1,13 @@
-// to do 
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main (int argc, char* argv[]) {
+  std::cout << "Hello" << std::endl;
+  
+  
+  return 0;
+}
+
+float[][] cv_to_cpp() {
+  float[][] out 
+}
